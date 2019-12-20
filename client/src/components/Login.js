2dda99ea99df4e2770d15b826fcd36e80746e97e
newbi462 <isthisemailfree@gmail.com>
2019-12-20 11:49:16 -0500

    Construct an AXIOS request to retrieve a token from the server. You'll use this token to interact with the API  Save the token to localStorage

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,14 +1,63 @@
-import React from "react";
+import React, { useState } from 'react';
 
-const Login = () => {
+import { axiosWithAuth } from "../auth/axiosWithAuth";
+
+export const Login = (props) => {
+  const [credentials, setCredentials] = useState(
+    {
+      username: "",
+      password: ""
+    }
+  );
+
+  const handleChange = (event) => {
+    setCredentials(
+      {
+        ...credentials,
+        [event.target.name]: event.target.value
+      }
+    );
+  };
+
   // make a post request to retrieve a token from the api
+  const login = (event) => {
+    event.preventDefault();
+    axiosWithAuth()
+      .post("/login", credentials)
+      .then(responce => {
+        console.log(responce);
+        localStorage.setItem("token", responce.data.payload);
+        props.history.push("/protected");
+      })
+      .catch(error => console.log(error));
+  };
+
+
+
   // when you have handled the token, navigate to the BubblePage route
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
       <p>Build a login page here</p>
-    </>
+      <form onSubmit={login}>
+          <input
+            type="text"
+            name="username"
+            value={credentials.username}
+            placeholder="User Name"
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            value={credentials.password}
+            placeholder="Password"
+            onChange={handleChange}
+          />
+          <button>Submit</button>
+        </form>
+      </>
   );
 };
 
-export default Login;
+//export default Login;
